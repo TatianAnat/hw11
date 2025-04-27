278f37889cb1755b8ef8d992d1a594d481946b4f
TatianAnat <TatianAnat@yandex.ru>
2025-04-27 14:50:50 +0400

    homework 11 task 2

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,26 @@ public class Main {
         }
         System.out.println("Сумма трат за месяц составила " + monthlyAmount + " рублей");
 
+        //task 2
+        System.out.println("Task 2");
+        int maximumAmount = 0;
+        int[] monthlyPaymentNew = new int [5];
+        monthlyPaymentNew[0] = 200;
+        monthlyPaymentNew[1] = 250;
+        monthlyPaymentNew[2] = 100;
+        monthlyPaymentNew[3] = 180;
+        monthlyPaymentNew[4] = 240;
+
+        int minimumAmount = monthlyPaymentNew[0];
+        for(int i = 0; i < monthlyPaymentNew.length; i++){
+            if (monthlyPaymentNew[i] < minimumAmount) {
+                minimumAmount = monthlyPaymentNew[i];
+            }
+            if (monthlyPaymentNew[i] > maximumAmount) {
+                maximumAmount = monthlyPaymentNew[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + minimumAmount + " рублей. Максимальная сумма трат за неделю составила " + maximumAmount + " рублей.");
 
     }
 }
