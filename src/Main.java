98b94fe0c7693f6c7d2ddcd68326411bafbdcbc2
TatianAnat <TatianAnat@yandex.ru>
2025-04-27 14:35:37 +0400

    homework 11 task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //task 1
+        System.out.println("Task 1");
+        int monthlyAmount = 0;
+        int[] monthlyPayment = new int [5];
+        monthlyPayment[0] = 200;
+        monthlyPayment[1] = 250;
+        monthlyPayment[2] = 100;
+        monthlyPayment[3] = 180;
+        monthlyPayment[4] = 240;
+        for(int i = 0; i < monthlyPayment.length; i++){
+            monthlyAmount = monthlyAmount + monthlyPayment[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + monthlyAmount + " рублей");
+
+
     }
 }
